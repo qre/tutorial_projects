3116a7ad9ff055d18e8bc6968e0c6ee6a6fee8ba
qre <zaikingleb@gmail.com>
2022-02-11 14:08:50 +0100

    Delete madlib.py

--- madlib.py
+++
@@ -1,9 +0,0 @@
-adj = input("Write an adjective")
-verb1 = input("Write a verb")
-verb2 = input("Write another verb")
-person = input("Write the name of a famous person")
-
-madlib = f"Python is so {adj}! It's exciting too because i love to {verb1}. Anyway,\
-stay safe and {verb2} like you are {person}"
-
-print(madlib)
