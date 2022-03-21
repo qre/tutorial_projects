33d801eaa673b384c8ed3401655c3dca6915e841
qre <zaikingleb@gmail.com>
2022-03-21 12:07:02 +0100

    Add files via upload

---
+++ test_madlib.py
@@ -0,0 +1,5 @@
+import madlib
+
+if madlib.adj == 'cool' and madlib.verb1 == 'VERB!' and madlib.verb2 == 'vErB2' and madlib.person == 'me':
+    assert madlib.madlib == 'Python is so cool! It is exciting too because i love to VERB!. Anyway,\
+stay safe and vErB2 like you are me'
