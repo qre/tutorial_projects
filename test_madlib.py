8951dd67e2eaa040a900ebd6f2d88dbe3774ff1b
qre <zaikingleb@gmail.com>
2022-03-22 11:20:14 +0100

    Update test_madlib.py

--- test_madlib.py
+++ test_madlib.py
@@ -1,5 +1,6 @@
 import madlib
 
-if madlib.adj == 'cool' and madlib.verb1 == 'VERB!' and madlib.verb2 == 'vErB2' and madlib.person == 'me':
-    assert madlib.madlib == 'Python is so cool! It is exciting too because i love to VERB!. Anyway,\
+def test_madlib():
+    if madlib.adj == 'cool' and madlib.verb1 == 'VERB!' and madlib.verb2 == 'vErB2' and madlib.person == 'me':
+        assert madlib.madlib == 'Python is so cool! It is exciting too because i love to VERB!. Anyway,\
 stay safe and vErB2 like you are me'
